227243d0bb9cfc51f2eaacd61fe836e337810b0f
Peter Engelbrecht <peter@engelbrecht.dk>
2026-01-09 09:23:23 +0000

    Update formula to use homebrew-tap releases

--- Formula/spe2ed.rb
+++ Formula/spe2ed.rb
@@ -9,14 +9,14 @@ class Spe2ed < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pengelbrecht/spe2ed/releases/download/v#{version}/spe2ed_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/pengelbrecht/homebrew-tap/releases/download/v#{version}/spe2ed_#{version}_darwin_arm64.tar.gz"
       sha256 "0edff4e25a458900b7f8943f4fdca0e03b609bb1c253ff2c8c5595b8fa3b4177"
 
       def install
         bin.install "spe2ed"
       end
     else
-      url "https://github.com/pengelbrecht/spe2ed/releases/download/v#{version}/spe2ed_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/pengelbrecht/homebrew-tap/releases/download/v#{version}/spe2ed_#{version}_darwin_amd64.tar.gz"
       sha256 "a5881e26b47d3a909e37113cd1b7bb7f6362ea386458c1a22c9de01ece3df38f"
 
       def install
@@ -27,14 +27,14 @@ class Spe2ed < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/pengelbrecht/spe2ed/releases/download/v#{version}/spe2ed_#{version}_linux_arm64.tar.gz"
+      url "https://github.com/pengelbrecht/homebrew-tap/releases/download/v#{version}/spe2ed_#{version}_linux_arm64.tar.gz"
       sha256 "27016e55b6a7ebf8bad5a074eba6dc4e3427d35573d699c752e0ed96e953a01f"
 
       def install
         bin.install "spe2ed"
       end
     else
-      url "https://github.com/pengelbrecht/spe2ed/releases/download/v#{version}/spe2ed_#{version}_linux_amd64.tar.gz"
+      url "https://github.com/pengelbrecht/homebrew-tap/releases/download/v#{version}/spe2ed_#{version}_linux_amd64.tar.gz"
       sha256 "1ce50638807beda185948fc658216f58f741731dcfa40164df9606ff7124b2b8"
 
       def install
